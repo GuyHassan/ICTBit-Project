28342f233a738453d633ab89ab248a8a2ebc76fb
Guy Hassan <33221427+GuyHassan@users.noreply.github.com>
2021-06-05 14:38:29 +0300

    Fixing some comments, create a Mongoose files.

--- updateCurrentUser.js
+++ updateCurrentUser.js
@@ -1,5 +1,43 @@
-// updateCurrentUser = (currentUserID) => {
-//     try {
-//   };
+const mongoose = require("mongoose");
+const userSchema = require('./userModel');
+const pathMongo = "ictbit-mongo ;)";
 
-// Please comment out all your code when you are finished.
+// Connect to mongo
+const initialMongo = async () => {
+    await mongoose.connect(pathMongo, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    return mongoose
+}
+
+
+const updateCurrentUser = async (currentUserID) => {
+    try {
+        //First element is a filter (searching the currentID), second element is update the element and in the end return the new updated record.
+        return await userSchema.findOneAndUpdate(
+            {
+                ID: currentUserID
+            },
+            {
+                ID: 'newId'
+            })
+    } finally {
+        console.log('The user is not found !');
+    }
+};
+
+// Raises the db to work.
+const connectToMongo = async () => {
+    await initialMongo().then(mongoose => {
+        try {
+            console.log('Connected to Mongo');
+            let newUserAfterUpdate = updateCurrentUser()
+        } finally {
+            mongoose.connection.close()
+        }
+    })
+}
+
+connectToMongo();
+
--- userModel.js
+++ userModel.js
@@ -1,11 +1,28 @@
-// const mongoose = require("mongoose");
-// const Schema = mongoose.Schema;
 
-// const UserSchema = new Schema({
-// });
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//Defining types
+const types = (nType) => {
+    return {
+        type: nType,
+        required: true
+    }
+}
+/**
+ * timestamps - On each update or new record he - add 2 values to the record that include the current date and time
+ * Name and Age is same as Usermodel.ts
+ * and ID is to search the current user, because name there is no primary key
+ */
+const UserSchema = new Schema({
+    Name: types(String),
+    Age: types(Number),
+    ID: types(Number)
+}, {
+    timestamps: true
+});
 
 
-// module.exports = mongoose.model);
+module.exports = mongoose.model('users', UserSchema);
 
 
-// Please comment out all your code when you are finished.
